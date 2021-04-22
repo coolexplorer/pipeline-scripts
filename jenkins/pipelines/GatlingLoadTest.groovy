3174ee5d582741e2bb8307f9b4077565451dd605
seunkim <seunkim@ea.com>
2021-04-22 20:21:56 +0900

    Added k8s agent yaml file and updated pipelin script

--- jenkins/pipelines/GatlingLoadTest.groovy
+++ jenkins/pipelines/GatlingLoadTest.groovy
@@ -1,7 +1,16 @@
 pipeline {
-    node('default') {
-        stage('test') {
-            sh 'echo hello world'
+    agent {
+        kubernetes {
+            yamlFile 'agent/k8s/maven.yaml'
+        }
+    }
+    stages {
+        stage('Run maven version') {
+            steps {
+                container('maven') {
+                    sh 'mvn --version'
+                }
+            }
         }
     }    
 }
