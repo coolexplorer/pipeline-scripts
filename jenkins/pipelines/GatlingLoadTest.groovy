3e616b419a8e28c83cd9491410734364a7085fba
seunkim <seunkim@ea.com>
2021-05-14 00:32:37 +0900

    Added gatling report genration stage

--- jenkins/pipelines/GatlingLoadTest.groovy
+++ jenkins/pipelines/GatlingLoadTest.groovy
@@ -1,5 +1,7 @@
-reportPath = "target/gatling/report"
-resultPath = "target/gatling"
+#!/usr/bin/env groovy
+
+def reportPath = "target/gatling/report"
+def resultPath = "target/gatling"
 
 pipeline {
     agent {
@@ -46,7 +48,7 @@ pipeline {
                 container('maven') {
                     dir('maven-gatling') {
                         
-                        simulationPath = sh script: "ls -l ${resultPath} | grep loadtestsimulation | awk \'{print \$10}\'", returnStdout: true
+                        def simulationPath = sh script: "ls -l ${resultPath} | grep loadtestsimulation | awk \"{print \$10}\"", returnStdout: true
                         echo "${simulationPath}"
                         sh "cp ${resultPath}/${simulationPath}/*.log ${reportPath}"
 
