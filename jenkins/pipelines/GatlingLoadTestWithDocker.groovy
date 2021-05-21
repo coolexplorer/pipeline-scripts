d3a76d70a5c58176f1e600a22596225b290f8cb4
seunkim <seunkim@ea.com>
2021-05-21 12:18:54 +0900

    Added gatling test run stage with docker

--- jenkins/pipelines/GatlingLoadTestWithDocker.groovy
+++ jenkins/pipelines/GatlingLoadTestWithDocker.groovy
@@ -27,18 +27,32 @@ pipeline {
                     dir('maven-gatling') {
                         script {
                             def loadProfile = """
-                            #!/bin/bash
-                            export TARGET_SERVER=${params.TargetServer}
-                            export USERS=${params.Users}
-                            export DURATION=${params.Duration}
-                            export RAMPUP_DURATION=${params.RampUpDuration}
-                            export DURATION_UNIT=${params.DurationUnit}
-                            export SCENARIO=${params.Scenario}
+                            TARGET_SERVER=${params.TargetServer}
+                            USERS=${params.Users}
+                            DURATION=${params.Duration}
+                            RAMPUP_DURATION=${params.RampUpDuration}
+                            DURATION_UNIT=${params.DurationUnit}
+                            SCENARIO=${params.Scenario}
                             """.stripIndent()
 
                             echo "Environment variables : ${loadProfile}"
 
-                            writeFile file: "./load_profile.sh", text: loadProfile
+                            writeFile file: "./load_profile.env", text: loadProfile
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Run Gatling test') {
+            steps {
+                container('docker') {
+                    dir('maven-gatling') {
+                        script {
+                            echo "cat ./load_profile.env"
+                            def docker_command = "docker run -ulimit nofile=20480:20480 --env-file ./load_profile.env --name=loadgen coolexplorer/maven-gatling:latest"
+                            def gatling_command = "bash -C \"mvn gatling:test\""
+                            sh "${docker_command} ${gatling_command}"
                         }
                     }
                 }
