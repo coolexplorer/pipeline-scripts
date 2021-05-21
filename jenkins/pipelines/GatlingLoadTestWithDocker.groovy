b642fd3c1fd842f77ccaf38756c3b3ab13c9b911
seunkim <seunkim@ea.com>
2021-05-21 23:45:37 +0900

    Updated docker command

--- jenkins/pipelines/GatlingLoadTestWithDocker.groovy
+++ jenkins/pipelines/GatlingLoadTestWithDocker.groovy
@@ -73,7 +73,7 @@ pipeline {
                             sh "cat ./load_profile.env"
 
                             def docker_command = "docker run --rm --network host --ulimit nofile=20480:20480 --env-file ./load_profile.env --name=${DOCKER_NAME} -v \"${WORKSPACE}/${repositoryPath}\":\"/${testResultPath}/target\" ${params.Image}"
-                            def gatling_command = "bash -c \"mvn gatling:test;chmod 777 -R /gatling-test/target\""
+                            def gatling_command = "bash -c \"mvn gatling:test -Dgatling.noReports=true;mkdir -p /${testResultPath}/target/report;mvn gatling:test -Dgatling.reportsOnly=report\""
 
                             sh "${docker_command} ${gatling_command}"
                         }
@@ -82,18 +82,18 @@ pipeline {
             }
         }
 
-        stage('Generate Report') {
-            steps {
-                container('maven') {
-                    dir("${repositoryPath}") {
-                        script {
-                            sh "mkdir -p target/gatling/report"
-                            sh 'mvn gatling:test -Dgatling.noReports=true'
-                        }
-                    }
-                }
-            }
-        }
+        // stage('Generate Report') {
+        //     steps {
+        //         container('maven') {
+        //             dir("${repositoryPath}") {
+        //                 script {
+        //                     sh "mkdir -p target/gatling/report"
+        //                     sh 'mvn gatling:test -Dgatling.reportsOnly=report'
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
 
         stage('Publish gatling report') {
             steps {
