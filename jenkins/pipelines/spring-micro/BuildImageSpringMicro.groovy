b46e535f3ba3ba9723bda1cc480e98413c723afc
seunkim <seunkim@ea.com>
2022-03-05 22:05:42 -0800

    [pipeline-script] Add release and building image jobs for spring-micro-gateway

--- jenkins/pipelines/spring-micro/BuildImageSpringMicro.groovy
+++ jenkins/pipelines/spring-micro/BuildImageSpringMicro.groovy
@@ -20,7 +20,7 @@ pipeline {
             }
         }
 
-        stage('Build auth') {
+        stage('Build') {
             steps {
                 container('maven') {
                     dir("${params.ProjectName}") {
